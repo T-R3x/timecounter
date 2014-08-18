c146cf34e0d80d9c50c1c0975668910d1991dfad
PhR3Ak <hofrichter.sascha@gmail.com>
2014-08-18 20:16:34 +0200

    create props, getters and setters

--- TimeCounter.js
+++ TimeCounter.js
@@ -5,5 +5,59 @@
  *
  * @param boolean
  */
-function TimeCounter(autostart) {}
+function TimeCounter(autostart) {
+    var autostart = autostart || false;
 
+    /**
+     * Private members
+     */
+    var count = 1;
+    var hours = 0;
+    var minutes = 0;
+    var secs = 0;
+
+    var _intervalTimer;
+
+    /**
+     * Define getters and setters
+     */
+    Object.defineProperty(this, 'count', {
+        get: function () {
+            return count;
+        },
+        set: function (value) {
+            count = value;
+        }
+    });
+
+    Object.defineProperty(this, 'hours', {
+        get: function () {
+            return hours;
+        },
+        set: function (value) {
+            hours = value;
+        }
+    });
+
+    Object.defineProperty(this, 'minutes', {
+        get: function () {
+            return minutes;
+        },
+        set: function (value) {
+            minutes = value;
+        }
+    });
+
+    Object.defineProperty(this, 'secs', {
+        get: function () {
+            return secs;
+        },
+        set: function (value) {
+            secs = value;
+        }
+    });
+
+    if (autostart) {
+        console.debug('Do something');
+    }
+}
